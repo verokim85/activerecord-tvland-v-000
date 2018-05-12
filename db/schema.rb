10b3484803eb90778c8021d0fd6704d96f19d193
Veronica Kim <vero.a.kim@gmail.com>
2018-05-12 23:51:48 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 10) do
+ActiveRecord::Schema.define(version: 11) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 10) do
     t.integer "network_id"
     t.string "genre"
     t.string "network"
+    t.string "day"
+    t.string "season"
   end
 
 end
